d7795437d72812d27fe444473a6c74567024ee2c
Burhan Uddin <bunnub5683@gmail.com>
2022-04-17 15:36:20 +0600

    about me route added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+const style = {
+    textAlign: 'center',
+    height: '50vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '1.2rem',
+}
 const About = () => {
     return (
-        <div>
-            this is about
+        <div style={style}>
+            <div>
+                <h1>I'm Burhan Uddin</h1>
+                <p>I'm a student. I passed Higher Secondary level in 2021. <br />
+                    I'm preparing myself for university admission. <br /> Besides I'm learning web development from Jhankar Mahbub sir. <br /> My goal is to become CEO of Google or Microsoft. -_- <br />
+                    I love to learn new things and I'm always curious about unknown things <br />
+                </p>
+
+            </div>
         </div>
     );
 };
